a251154ad926431ea0f9ae3882503e96b515811b
Favian Junnanda Zahri <89818801+FavianJZ@users.noreply.github.com>
2026-08-08 22:26:35 +0700

    Update verify-captcha.js

--- api/verify-captcha.js
+++ api/verify-captcha.js
@@ -1,19 +1,17 @@
-
 export default async function handler(req, res) {
-
   if (req.method !== 'POST') {
     return res.status(405).json({ success: false, message: 'Method Not Allowed' });
   }
 
-  const { token, action } = req.body;
+  const { token } = req.body;
   
   if (!token) {
     return res.status(400).json({ success: false, message: 'Token is missing' });
   }
 
   const PROJECT_ID = 'my-project-example-samples'; 
-  const SITE_KEY = '6LeqqHstAAAAAOZJk-wusa0Cxq5n7vQyi4rvRFJ9';
-
+  const SITE_KEY = '6Lf6snstAAAAAITdD8GWZkYM3cg4piXdOwA0zTb5'; // Key Checkbox Baru
+  
   const API_KEY = process.env.GOOGLE_API_KEY; 
 
   const url = `https://recaptchaenterprise.googleapis.com/v1/projects/${PROJECT_ID}/assessments?key=${API_KEY}`;
@@ -25,7 +23,6 @@ export default async function handler(req, res) {
       body: JSON.stringify({
         event: {
           token: token,
-          expectedAction: action,
           siteKey: SITE_KEY,
         }
       })
@@ -33,12 +30,9 @@ export default async function handler(req, res) {
 
     const data = await response.json();
 
+    // Untuk versi Checkbox, kita hanya perlu mengecek apakah token valid
     if (data.tokenProperties && data.tokenProperties.valid) {
-      if (data.riskAnalysis && data.riskAnalysis.score >= 0.5) {
-        return res.status(200).json({ success: true, score: data.riskAnalysis.score, message: 'Human verified' });
-      } else {
-        return res.status(403).json({ success: false, score: data.riskAnalysis?.score, message: 'Bot behavior detected' });
-      }
+        return res.status(200).json({ success: true, message: 'Human verified' });
     } else {
       return res.status(400).json({ success: false, message: 'Invalid token', details: data });
     }
